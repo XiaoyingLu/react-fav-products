823f7b41c08abc7a3eb25352bfceeecacfc372dc
Amber Lu <amber.lu@ubisoft.com>
2022-03-24 23:12:20 -0300

    replace redux with Context API

--- src/containers/Favorites.js
+++ src/containers/Favorites.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { useSelector } from 'react-redux';
 
 import FavoriteItem from '../components/Favorites/FavoriteItem';
+import { ProductsContext } from '../context/products-context';
 import './Products.css';
 
 const Favorites = props => {
-  const favoriteProducts = useSelector(state =>
-    state.shop.products.filter(p => p.isFavorite)
-  );
+  const favoriteProducts = useContext(ProductsContext).products.filter(p => p.isFavorite);
+
   let content = <p className="placeholder">Got no favorites yet!</p>;
   if (favoriteProducts.length > 0) {
     content = (
--- src/index.js
+++ src/index.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
-import './index.css';
-import App from './App';
-import productReducer from './store/reducers/products';
-
-const rootReducer = combineReducers({
-  shop: productReducer
-});
-
-const store = createStore(rootReducer);
+import "./index.css";
+import App from "./App";
+import ProductsProvider from "./context/products-context";
 
 ReactDOM.render(
-  <Provider store={store}>
+  <ProductsProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
+  </ProductsProvider>,
+  document.getElementById("root")
 );
