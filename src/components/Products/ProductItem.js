f84d448fa7bbc88a4010caf3cc861bc8ec769923
Amber Lu <amber.lu@ubisoft.com>
2022-03-24 23:59:38 -0300

    use custom store hook

--- src/components/Products/ProductItem.js
+++ src/components/Products/ProductItem.js
@@ -1,14 +1,14 @@
-import React, { useContext } from "react";
+import React from "react";
 
 import Card from "../UI/Card";
 import "./ProductItem.css";
-import { ProductsContext } from "../../context/products-context";
+import { useStore } from "../../hooks-store/store";
 
-const ProductItem = (props) => {
-  const toggleFav = useContext(ProductsContext).toggleFav;
+const ProductItem = React.memo((props) => {
+  const dispatch = useStore(false)[1];
 
   const toggleFavHandler = () => {
-    toggleFav(props.id);
+    dispatch("TOGGLE_FAV", props.id);
   };
 
   return (
@@ -25,6 +25,6 @@ const ProductItem = (props) => {
       </div>
     </Card>
   );
-};
+});
 
 export default ProductItem;
